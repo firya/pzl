9976a15908eee5d20bd5f5694940c28b6d7e631b
lebedevm <lebedevm@ozon.ru>
2024-10-14 15:48:46 +0300

    First commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './style.css'
+import { Application } from 'pixi.js';
+
+const app = new Application();
+
+await app.init({ background: '#1099bb', resizeTo: window });
+document.body.appendChild(app.canvas);
