2424dfe802946cc8e3022717e0b2a61043ebb80c
lebedevm <lebedevm@ozon.ru>
2024-10-29 23:29:06 +0300

    move world to separate class

--- src/levels/beginning/beginning.ts
+++ src/levels/beginning/beginning.ts
@@ -1,10 +1,9 @@
-import { Assets, Container, Sprite } from 'pixi.js';
+import { Assets } from 'pixi.js';
 
 import manifest from './manifest.json';
-import { App, eventEmitter } from '@/main.ts';
-import { Position } from '@/hero/hero.ts';
+import { LevelMap, World } from '@/world/world.ts';
 
-const levelMap = [
+const levelMap: LevelMap = [
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
   [0, 1, 0, 0, 1, 1, 1, 0, 1, 0],
@@ -12,73 +11,13 @@ const levelMap = [
   [0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 ];
-const tileSize = 500;
 
 export class Beginning {
-  private levelContainer: Container;
+  private world: World;
 
   constructor() {
-    this.init();
-  }
-
-  async init() {
-    eventEmitter.on('checkHeroPosition', this.checkCollision.bind(this));
-
-    this.levelContainer = new Container();
-
     Assets.init({ manifest });
     Assets.backgroundLoadBundle(['begin_screen']);
-
-    this.createLevel();
-
-    App.stage.addChildAt(this.levelContainer, 0);
-  }
-
-  async createLevel() {
-    for (let i = 0; i <= levelMap.length - 1; i++) {
-      for (let j = 0; j <= levelMap[i].length - 1; j++) {
-        const tile = levelMap[i][j];
-        const texture = tile
-          ? await Assets.load('grass')
-          : await Assets.load('grass2');
-        const sprite = new Sprite(texture);
-        sprite.position.set(j * tileSize, i * tileSize);
-        this.levelContainer.addChild(sprite);
-      }
-    }
-  }
-
-  checkCollision(oldPosition: Position, newPosition: Position, force = false) {
-    if (force) {
-      this.changeWorldPosition(newPosition);
-      return;
-    }
-
-    let positionResult = { ...newPosition };
-
-    if (newPosition.x < 0 || newPosition.x > levelMap[0].length * tileSize)
-      positionResult.x = oldPosition.x;
-    if (newPosition.y < 0 || newPosition.y > levelMap.length * tileSize)
-      positionResult.y = oldPosition.y;
-    if (!this.checkPosition(oldPosition.x, newPosition.y))
-      positionResult.y = oldPosition.y;
-    if (!this.checkPosition(newPosition.x, oldPosition.y))
-      positionResult.x = oldPosition.x;
-
-    this.changeWorldPosition(positionResult);
-  }
-
-  checkPosition(x: number, y: number) {
-    const tileX = Math.floor(x / tileSize);
-    const tileY = Math.floor(y / tileSize);
-
-    return levelMap[tileY][tileX];
-  }
-
-  changeWorldPosition(position: Position) {
-    if (!this.levelContainer) return;
-    eventEmitter.emit('changeHeroPosition', position);
-    this.levelContainer.x = -position.x + App.screen.width / 2;
-    this.levelContainer.y = -position.y + App.screen.height / 2;
+    this.world = new World(levelMap);
   }
 }
---
+++ src/world/world.ts
@@ -0,0 +1,75 @@
+import { Assets, Container, Sprite } from 'pixi.js';
+
+import { App, eventEmitter } from '@/main.ts';
+import { Position } from '@/hero/hero.ts';
+
+const tileSize = 500;
+export type LevelMap = Array<Array<number>>;
+
+export class World {
+  private levelContainer: Container;
+  private levelMap: LevelMap;
+
+  constructor(levelMap: LevelMap) {
+    this.levelMap = levelMap;
+    this.init();
+  }
+
+  async init() {
+    eventEmitter.on('checkHeroPosition', this.checkCollision.bind(this));
+
+    this.levelContainer = new Container();
+
+    this.createLevel();
+
+    App.stage.addChildAt(this.levelContainer, 0);
+  }
+
+  async createLevel() {
+    for (let i = 0; i <= this.levelMap.length - 1; i++) {
+      for (let j = 0; j <= this.levelMap[i].length - 1; j++) {
+        const tile = this.levelMap[i][j];
+        const texture = tile
+          ? await Assets.load('grass')
+          : await Assets.load('grass2');
+        const sprite = new Sprite(texture);
+        sprite.position.set(j * tileSize, i * tileSize);
+        this.levelContainer.addChild(sprite);
+      }
+    }
+  }
+
+  checkCollision(oldPosition: Position, newPosition: Position, force = false) {
+    if (force) {
+      this.changeWorldPosition(newPosition);
+      return;
+    }
+
+    let positionResult = { ...newPosition };
+
+    if (newPosition.x < 0 || newPosition.x > this.levelMap[0].length * tileSize)
+      positionResult.x = oldPosition.x;
+    if (newPosition.y < 0 || newPosition.y > this.levelMap.length * tileSize)
+      positionResult.y = oldPosition.y;
+    if (!this.checkPosition(oldPosition.x, newPosition.y))
+      positionResult.y = oldPosition.y;
+    if (!this.checkPosition(newPosition.x, oldPosition.y))
+      positionResult.x = oldPosition.x;
+
+    this.changeWorldPosition(positionResult);
+  }
+
+  checkPosition(x: number, y: number) {
+    const tileX = Math.floor(x / tileSize);
+    const tileY = Math.floor(y / tileSize);
+
+    return this.levelMap[tileY][tileX];
+  }
+
+  changeWorldPosition(position: Position) {
+    if (!this.levelContainer) return;
+    eventEmitter.emit('changeHeroPosition', position);
+    this.levelContainer.x = -position.x + App.screen.width / 2;
+    this.levelContainer.y = -position.y + App.screen.height / 2;
+  }
+}
